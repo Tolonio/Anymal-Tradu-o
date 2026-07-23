69d0694e6387028bb36a1edb305186fd29aea0ce
Tolonio <138166141+Tolonio@users.noreply.github.com>
2026-07-23 16:54:31 -0300

    Update servidor_web.py

--- codigos para o anymal/servidor_web.py
+++ codigos para o anymal/servidor_web.py
@@ -52,4 +52,4 @@ def traduzir_missao():
 
 if __name__ == '__main__':
     print("=== Servidor Nativo Iniciado na porta 5000 ===")
-    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=5050, debug=False, threaded=True)
